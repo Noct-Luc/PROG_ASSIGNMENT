daeafb5f178f1cbcc21f2ab019471e22f245f915
J Calvert <zanoftheworld@gmail.com>
2025-09-08 11:31:16 +0000

    Update DreamerSleptTest.java

--- PROG_Assignment/Presentation/src/DreamerSleptTest.java
+++ PROG_Assignment/Presentation/src/DreamerSleptTest.java
@@ -1,34 +1,54 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DreamerSleptTest {
+class dreamerSleptTest {
 
-    @Test
-    void setDreamScape() {
+    DreamerSlept dreamer;
+
+    @BeforeEach
+    void setUp() {
+        dreamer = new DreamerSlept(7, "Starfall City");
     }
 
     @Test
-    void getDreamNum() {
+    void TestSetDreamScape() {
+        dreamer.setDreamScape("Moonlight Meadows");
+        assertEquals("Moonlight Meadows", dreamer.getDreamScape());
     }
 
     @Test
-    void getDreamScape() {
+    void TestGetDreamNum() {
+        assertEquals(7, dreamer.getDreamNum());
     }
 
     @Test
-    void dreamRank() {
+    void TestGetDreamScape() {
+        assertEquals("Starfall City", dreamer.getDreamScape());
     }
 
     @Test
-    void testGetDreamNum() {
+    void TestDreamRank_Bronze() {
+        String rank = DreamerSlept.dreamRank(3, "Dark Forest");
+        assertEquals("Bronze", rank);
     }
 
     @Test
-    void testGetDreamScape() {
+    void TestDreamRank_Silver() {
+        String rank = DreamerSlept.dreamRank(7, "Sky Haven");
+        assertEquals("Silver", rank);
     }
 
     @Test
-    void getDreamerProcess() {
+    void TestDreamRank_Gold() {
+        String rank = DreamerSlept.dreamRank(12, "Crystal Peaks");
+        assertEquals(" Gold", rank); // note the space in front of "Gold" in your original code
+    }
+
+    @Test
+    void TestGetDreamerProcess() {
+        DreamerSlept.dreamRank(9, "Frozen Tundra");
+        assertEquals("Silver", dreamer.getDreamerProcess());
     }
 }
